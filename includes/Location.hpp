23b6188fb066e825209cb0c691263c1b25cf0b4a
Ozziur <casaruizzo@yahoo.it>
2023-05-11 13:48:26 +0200

    pausa pranzo

---
+++ includes/Location.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Location.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mruizzo <mruizzo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 13:16:58 by mruizzo           #+#    #+#             */
+/*   Updated: 2023/05/11 13:18:07 by mruizzo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+
+#include <iostream>
+#include <vector>
+
+class Location
+{
+	private:
+
+	public:
+		Location();
+		~Location();
+};
+
+#endif
