015d46c8890d47161cdb3df8a2653066510ddbaa
Alice Novelli <anovelli@e3r2p1.42roma.it>
2023-05-17 16:51:13 +0200

    stiamo provando a lasciare il server attivo anche quando il client si chiude

--- temp_main.cpp
+++ temp_main.cpp
@@ -1,7 +1,26 @@
+#define RESET       =   "\033[0m" 
+#define BLACK       =   "\033[30m"
+#define RED         =   "\033[31m"
+#define GREEN       =   "\033[32m"
+#define YELLOW      =   "\033[33m"
+#define BLUE        =   "\033[34m"
+#define MAGENTA     =   "\033[35m"
+#define CYAN        =   "\033[36m"
+#define WHITE       =   "\033[37m"
+#define BOLDBLACK   =   "\033[1m\033[30m"
+#define BOLDRED     =   "\033[1m\033[31m"
+#define BOLDGREEN   =   "\033[1m\033[32m"
+#define BOLDYELLOW  =   "\033[1m\033[33m"
+#define BOLDBLUE    =   "\033[1m\033[34m"
+#define BOLDMAGENTA =   "\033[1m\033[35m"
+#define BOLDCYAN    =   "\033[1m\033[36m"
+#define BOLDWHITE   =   "\033[1m\033[37m"
+
 #include "includes/Server.hpp"
 #include <stdlib.h>
 #include <signal.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 /* Queste due funzioni servono a liberare la roba in caso di
 ** Ctrl + C, che sarà una causa abbastanza comune di interruzione
@@ -43,6 +62,7 @@ int	main(int argc, char **argv)
 		delete (testServer);
 		return (1);
 	}
+	signal(SIGPIPE, SIG_IGN);//La chiamata a signal(SIGPIPE, SIG_IGN) imposta il gestore dei segnali per il segnale SIGPIPE in modo da ignorare il segnale quando viene ricevuto, anziché gestirlo con il comportamento predefinito del sistema operativo, che di solito è terminare il processo.
 	server_deleter(testServer);
 	memset( &sa, 0, sizeof(sa) );
 	sa.sa_handler = sigint_handler;
@@ -58,23 +78,32 @@ int	main(int argc, char **argv)
 		if (temp_sock < 0)
 		{
 			std::cerr << "accept() failed." << std::endl;
-			exit(0);
+			// exit(0);
 		}
 		pid = fork();
-		if (pid < 0)
+		if (pid == 0)
 		{
-			std::cerr << "fork() failed" << std::endl;
+			close(temp_sock);
+		}
+		else
+		{
+						// std::cerr << "fork() failed" << std::endl;
 			std::string	str;
 			char		buff[1];
 			delete (testServer);
-			while (read(temp_sock, buff, 1) == 1)
+			int temp;
+			while ((temp = read(temp_sock, buff, 1)) == 1)
+			{
 				str.push_back(*buff);
+			}
 			write(temp_sock, str.c_str(), str.size());
 			std::cout << temp_sock << std::endl;
 			exit(0);
 		}
-		else
-			close(temp_sock);
+		    // Raccolta dei processi figli completati
+		while (waitpid(-1, NULL, WNOHANG) > 0)
+			;
+
 	}
 	delete (testServer);
 	return (0);
