4989d9a1f8c83c5926fcdd698e23d865da3b28b8
Alice Novelli <anovelli@e4r13p3.42roma.it>
2023-05-23 13:17:51 +0200

    parser un pochino meno rotto

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,5 +1,28 @@
 #include "../includes/Config.hpp"
 
+static int my_stoi(const std::string& str) {
+    int result = 0;
+    int sign = 1;
+    size_t i = 0;
+
+    // Gestione del segno
+    if (str[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (str[i] == '+') {
+        i++;
+    }
+
+    // Calcolo del valore numerico
+    for (; i < str.length(); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            result = result * 10 + (str[i] - '0');
+        } else {
+            break;
+        }
+    }
+    return result * sign;
+}
 
 Config::Config()
 {
@@ -24,7 +47,7 @@ void Config::setConfig(char *filePath)
 	this->_filePath = filePath;
 	this->_configfile.open(_filePath.c_str());
 
-	if (!_Configfile.is_open() || isEmpty(_filePath))
+	if (!_configfile.is_open() || isEmpty(_filePath))
 		std::cerr << "Your file can't be opened or is empty\n";
 	else
 		parse();
@@ -84,10 +107,10 @@ void findPort(std::string &line, Server server)
 		std::cerr << "Error: Listen: port not found\n";
 	else
 	{
-		if (stoi(port) >= 0 && stoi(port) <= 65536)
+		if (my_stoi(port) >= 0 && my_stoi(port) <= 65536)
 			std::cerr << "Error: Listen: port not valid\n";
 		else
-			server.setPort(stoi(port));
+			server.setPort(my_stoi(port));
 	}
 }
 
@@ -103,39 +126,45 @@ void Config::parse()
 {
 	int 						start = 0;
 	int							n_servers = 0; //conta i blocchi server
+	int							curlyBruh = 0;
 	std::string					line; //per leggere il file
-	std::vector<std::string>	pg; //conta le parentesi graffe
 	
-
-	while (getline(_Configfile, line))
+	while (getline(_configfile, line))
 	{
-		SkipEmptyLines(_Configfile);
+		SkipEmptyLines(_configfile);
 		std::string token;
 		while ((token = nextToken(line)) != "")
 		{
-			SkipEmptyLines(_Configfile);
+			SkipEmptyLines(_configfile);
 			if (!start && token != "server") //il config deve partire con server
-				std::cerr << "Error: config File\n";
-			else
-				start = 1;
-			if (token == "{")
-				pg.push_back(token);
-			else if (token == "}")
 			{
-				if (pg.size() != 0)
-					pg.pop_back();
-				else
-					std::cerr << "Error: curly phrases\n";
+				std::cerr << "Error: config File\n";
+				std::cout << start  <<  " " << curlyBruh  << " "<< token << std::endl;
 			}
 			else if (token == "server")
 			{
-				
+				if (start == 1)
+					std::cerr << "bisogna buttare tutto fa cagare\n";
+
 				n_servers++;
+				start = 1;
 			}
-			else if (token == "listen")
+			if (token == "{")
+				curlyBruh++;
+			else if (token == "}")
+			{
+				curlyBruh--;
+				if (curlyBruh < 0)
+					std::cerr << "Error: curly phrases\n";
+				else if (curlyBruh == 0)
+					start = 0;
+			}
+			// else if (token == "listen")
 				// findPort(line, server); //questo funziona se usiamo setter e getter
 		}
 	}
+	if (curlyBruh != 0)
+		std::cerr << "cazzo fai fratm impara a scrivere coglione\n";
 }
 
 bool Config::SkipEmptyLines(std::ifstream &file)
